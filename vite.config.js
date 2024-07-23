14b1113bbccbfdfb506612e8f9e22392160ed5c5
gokyoungeun <gke3364@outspring.co.kr>
2024-07-23 16:31:32 +0900

    수정

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '192.168.1.134',  // 또는 특정 IP 주소로 변경 가능
+    port: 5173
+  }
 })
