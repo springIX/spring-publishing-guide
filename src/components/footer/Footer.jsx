22cfb0bbf650dbc4445c64ca0ccffe48b2a3a803
gokyoungeun <gke3364@outspring.co.kr>
2024-07-24 15:05:24 +0900

    footer 수정

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,9 +10,7 @@ const footer = () => {
             <img src='https://www.outspring.co.kr/wp-content/uploads/2022/10/logo_light-1.png' alt='logo' />
           </div>
           <div className={styles.footer_info}>
-            <p>서울특별시 서초구 서초대로 396, 3층</p>
-            <p>대표전화: 02-0000-0000</p>
-            <p>이메일:abc1234@abcd.com</p>
+            <p>2024.07.24</p>
           </div>
         </div>
       </div>
